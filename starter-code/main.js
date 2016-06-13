8d8299e61e9ca57baa174abf6f489795a34dae21
Noah Feder <stavro510@Noahs-MacBook-Air.local>
2016-06-13 18:22:31 -0400

    procedural div generation

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,21 @@
 var cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour = "king";
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert("Sorry, try again.");
 } else if (cardOne === cardTwo || cardThree == cardFour) {
 	alert("You found a match!");
-}
+}*/
+
+var createBoard = function() {
+
+	var board = document.getElementById('game-board');
+
+	for (var i = 0; i < 4; i++) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+
+};
+
+createBoard();

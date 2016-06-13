cdcffa89afd03c8aaa77e9829e252aa94f881e21
Noah Feder <stavro510@Noahs-MacBook-Air.local>
2016-06-13 18:01:17 -0400

    boole

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
 var cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.");
+} else if (cardOne === cardTwo || cardThree == cardFour) {
+	alert("You found a match!");
+}

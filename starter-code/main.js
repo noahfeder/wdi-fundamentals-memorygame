380460be58b7e7944290f645f169b6c2f86ad7b0
Noah Feder <stavro510@Noahs-MacBook-Air.local>
2016-06-14 10:46:56 -0400

    final

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,4 @@
-var cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour = "king";
+var cards = ['queen','queen','king','king'], cardsInPlay = [];
 
 /*if (cardTwo === cardFour) {
 	alert("Sorry, try again.");
@@ -10,12 +10,46 @@ var createBoard = function() {
 
 	var board = document.getElementById('game-board');
 
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0, l = cards.length; i < l; i++) {
 		var card = document.createElement('div');
 		card.className = 'card';
+		card.setAttribute('data-card', cards[i]);
+		card.addEventListener('click', isTwoCards);
 		board.appendChild(card);
 	}
 
 };
+
+var isMatch = function(cardsInPlay) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("winner");
+	} else {
+		alert("loser");
+	}
+	var queens = document.querySelectorAll('[data-card=queen]');
+	var kings = document.querySelectorAll('[data-card=king]');
+	for (var i = 0; i < 2; i++) {
+		queens[i].setAttribute('class', 'card');
+		kings[i].setAttribute('class', 'card');
+	}
+};
+
+var isTwoCards = function() {
+
+  cardsInPlay.push(this.getAttribute('data-card'));
+  this.className = 'card ' + this.getAttribute('data-card');
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+  }
+
+};
+
 
 createBoard();
